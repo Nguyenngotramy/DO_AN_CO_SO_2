a51e9a3daa44f9ae2b2016904514100f1195c4db
Nguyenngotramy <Nguyenngotramy2427@gmail.com>
2023-11-21 23:20:45 +0700

    connectdatabase

--- database/connect.php
+++ database/connect.php
@@ -1,1 +1,13 @@
-<?
+<? 
+const _DBname = 'trangsuc';
+const _User = 'root';
+const _Pw ='';
+const _Host = 'localhost';
+try{
+    $dsn = 'mysql:dbname'._DBname.';host='._Host;
+    $conn = new PDO($dsn,_User,_Pw);
+}catch(Error $e){
+    echo $e->getMessage();
+    die();
+}
+?>
